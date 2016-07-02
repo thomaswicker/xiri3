65850cc87320c69961f1c22e00119288821cc3cf
Thomas Wicker <thomas@thomaswicker.com>
2016-07-02 10:31:29 -0600

    initializing new repo for xiri v3

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+//Potion Gulp Setup file
+var autoprefixer 	= require('gulp-autoprefixer'),
+		cache         = require('gulp-cache'),
+		concat        = require('gulp-concat'),
+		connect 			= require('gulp-connect'),
+		gulp 					= require('gulp')
+		livereload 		= require('gulp-livereload'),
+		haml 					= require('gulp-ruby-haml'),
+		imagemin      = require('gulp-imagemin'),
+		minifycss 		= require('gulp-minify-css'),
+		notify        = require('gulp-notify'),
+		rename 				= require('gulp-rename'),
+		sass 					= require('gulp-ruby-sass'),
+		uglify        = require('gulp-uglify');
+
+// Compile Haml into HTML
+gulp.task('haml', function() {
+  gulp.src('src/views/**/*.haml')
+    .pipe(haml().on('error', function(e) { console.log(e.message); }))
+    .pipe(gulp.dest('./'))
+		.pipe(notify({ message: 'HAML task complete' }))
+		.pipe(livereload());
+});
+
+gulp.task('scss', function() {
+	return sass('src/scss/application.scss', { sourcemap: false })
+		.on('error', function (err) { console.log("ERROR: " + err.message); })
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest('css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('css'))
+		.pipe(notify({ message: 'SCSS task complete' }))
+		.pipe(livereload());
+});
+
+gulp.task('js', function() {
+	return gulp.src('src/js/**/*.js')
+		.pipe(concat('application.js'))
+		.pipe(gulp.dest('js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('js'))
+		.pipe(notify({ message: 'JS task complete' }))
+		.pipe(livereload());
+});
+
+gulp.task('images', function() {
+	return gulp.src('src/img/**/*.svg')
+		.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('img'))
+		.pipe(notify({ message: 'IMAGES task complete' }))
+		.pipe(livereload());
+});
+
+gulp.task('watch', function() {
+	livereload.listen();
+	gulp.watch('src/views/**/*.haml', ['haml']).on('change', livereload.changed);
+  gulp.watch('src/scss/**/*.scss', ['scss']).on('change', livereload.changed);
+  gulp.watch('src/js/**/*.js', ['js']).on('change', livereload.changed);
+  gulp.watch('src/img/**/*', ['images']).on('change', livereload.changed);
+	livereload.listen();
+});
+
+// Kick off
+gulp.task('default', ['watch'], function() {
+	gulp.start('haml', 'scss', 'js', 'images');
+});
---
+++ src/js/application.js
@@ -0,0 +1,117 @@
+/*
+     _    ____  ____  _     ___ ____    _  _____ ___ ___  _   _       _ ____
+    / \  |  _ \|  _ \| |   |_ _/ ___|  / \|_   _|_ _/ _ \| \ | |     | / ___|
+   / _ \ | |_) | |_) | |    | | |     / _ \ | |  | | | | |  \| |  _  | \___ \
+  / ___ \|  __/|  __/| |___ | | |___ / ___ \| |  | | |_| | |\  | | |_| |___) |
+ /_/   \_\_|   |_|   |_____|___\____/_/   \_\_| |___\___/|_| \_|  \___/|____/
+*/
+
+
+//Setting up the core Angular App - potion
+var xiri = angular.module('xiriApp', ['ngRoute']);
+
+/*
+  ____   ___  _   _ _____ ___ _   _  ____
+ |  _ \ / _ \| | | |_   _|_ _| \ | |/ ___|
+ | |_) | | | | | | | | |  | ||  \| | |  _
+ |  _ <| |_| | |_| | | |  | || |\  | |_| |
+ |_| \_\\___/ \___/  |_| |___|_| \_|\____|
+
+*/
+
+// ###################### BEGIN POTION'S ROUTING ###################### //
+
+xiri.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    // route for the default url home page
+    .when('/', {
+      templateUrl : 'partials/content.html',
+      controller  : 'MainController',
+      activePage  : 'home'
+    })
+}]);
+
+// ###################### END POTION'S ROUTING ###################### //
+
+
+
+/*
+   ____ ___  _   _ _____ ____   ___  _     _     _____ ____  ____
+  / ___/ _ \| \ | |_   _|  _ \ / _ \| |   | |   | ____|  _ \/ ___|
+ | |  | | | |  \| | | | | |_) | | | | |   | |   |  _| | |_) \___ \
+ | |__| |_| | |\  | | | |  _ <| |_| | |___| |___| |___|  _ < ___) |
+  \____\___/|_| \_| |_| |_| \_\\___/|_____|_____|_____|_| \_\____/
+*/
+
+// ###################### BEGIN POTION'S ANGULAR CONTROLLERS ########################## //
+
+// create the PotionController and inject Angular's $scope
+xiri.controller('MainController', ['$scope', '$route', 'xiriServices', function($scope, $route, xiriServices) {
+  $scope.message = 'Message from Home Page';
+}]);
+
+// create the HomeController and inject Angular's $scope
+xiri.controller('ContentController', ['$scope', '$route', 'xiriServices', function($scope, $route, xiriServices) {
+
+}]);
+//
+// // create the HomeController and inject Angular's $scope
+// xiri.controller('ComponentsController', ['$scope', '$route', 'xiriServices', function($scope, $route, xiriServices) {
+//
+// }]);
+
+
+// ###################### END POTION'S ANGULAR CONTROLLERS ##########################
+
+
+/*
+  ____ ___ ____  _____ ____ _____ _____     _______ ____
+ |  _ \_ _|  _ \| ____/ ___|_   _|_ _\ \   / / ____/ ___|
+ | | | | || |_) |  _|| |     | |  | | \ \ / /|  _| \___ \
+ | |_| | ||  _ <| |__| |___  | |  | |  \ V / | |___ ___) |
+ |____/___|_| \_\_____\____| |_| |___|  \_/  |_____|____/
+
+*/
+
+// ###################### BEGIN POTION'S ANGULAR CUSTOM DIRECTIVES ##################### //
+
+xiri.directive('superhero', function() {
+  return {
+    restrict: 'E',
+    link: function(scope, element) {
+
+      //Adding class to custom <superhero> element
+      element.addClass('superhero');
+    },
+    scope: {}
+  }
+});
+
+
+/*
+  ____  _____ ______     _____ ____ _____ ____
+ / ___|| ____|  _ \ \   / /_ _/ ___| ____/ ___|
+ \___ \|  _| | |_) \ \ / / | | |   |  _| \___ \
+  ___) | |___|  _ < \ V /  | | |___| |___ ___) |
+ |____/|_____|_| \_\ \_/  |___\____|_____|____/
+
+*/
+
+// ###################### BEGIN POTION'S ANGULAR SERVICES ###################### //
+
+xiri.service('xiriServices', function () {
+  var xiriObject = {};
+
+  // Function to call JS for adding default classes to new custom Angular directives
+  xiriObject.initDirectiveClasses = function () {
+
+    // Adding in variable to detect IE as user agent for specific styling
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+  };
+
+  // returning the potionObject within this services so that it can be accessed and initialized by Controllers
+  return xiriObject;
+});
+
+// ###################### END POTION'S ANGULAR SERVICES ######################### //
