6a9eb3d4dd02a65ea155077224e59865a0792c82
Thomas Wicker <thomas@thomaswicker.com>
2016-10-28 10:43:30 -0600

    removing livereload js and rewiring scroll to for nav

--- src/js/application.js
+++ src/js/application.js
@@ -86,6 +86,19 @@ xiri.directive('superhero', function() {
   }
 });
 
+xiri.directive('scrollToItem', function() {
+  return {
+      restrict: 'A',
+      scope: {
+        scrollTo: "@"
+      },
+      link: function(scope, $elm,attr) {
+        $elm.on('click', function() {
+            $('html,body').animate({scrollTop: $(scope.scrollTo).offset().top }, "slow");
+        });
+      }
+}});
+
 
 /*
   ____  _____ ______     _____ ____ _____ ____
